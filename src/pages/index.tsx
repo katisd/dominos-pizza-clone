97c9b908292c2cbeedb9c2a40747405c6119bbfa
katisd <90249534+katisd@users.noreply.github.com>
2022-12-17 02:21:57 +0700

    ✅ fix navbar and add container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
-import { NextPage } from "next";
+import type { NextPage } from "next";
 import React from "react";
+import Landing from "../Components/Landing";
 import NavBar from "../Components/NavBar";
 
 const Home: NextPage = () => {
   return (
-    <div className="min-h-screen bg-base-100">
+    <div className="h-screen overflow-hidden bg-base-100 ">
       <NavBar />
+      <Landing />
     </div>
   );
 };
