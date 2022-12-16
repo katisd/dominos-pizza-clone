359a820779a0c0dee5ab3bb1cd0953fed7abc7b7
katisd <90249534+katisd@users.noreply.github.com>
2022-12-17 02:18:23 +0700

    ✨ add Theme Provider(next Theme) ✨ add animate Presence(motion)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
+import { AnimatePresence } from "framer-motion";
+import { ThemeProvider } from "next-themes";
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <AnimatePresence>
+        <Component {...pageProps} />;
+      </AnimatePresence>
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
