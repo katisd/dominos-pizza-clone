c1de75d432e42bbc75e36f13724b53040519f504
katisd <90249534+katisd@users.noreply.github.com>
2022-12-18 19:18:07 +0700

    🚚 fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from "next";
 import React from "react";
 import Landing from "../Components/Landing";
-import NavBar from "../Components/NavBar";
+import NavBar from "../Components/Layouts/NavBar";
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto  h-screen overflow-hidden bg-base-100 ">
+    <div className="container mx-auto ">
       <NavBar />
       <Landing />
     </div>
--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,12 +1,23 @@
 import type { NextPage } from "next";
 import MenuList from "../Components/MenuList";
-import NavBar from "../Components/NavBar";
-
+import NavBar from "../Components/Layouts/NavBar";
+// import AboutPizza from "../Components/AboutPizza";
+// const testProps = {
+//   id: 1,
+//   name: "Pepperoni Passion",
+//   pic: "https://www.dominos.at/gallery/fmobile/2346ipar.png",
+//   type: "favorite",
+//   toppings: ["Tomatensauce", "Mozzarella", "Domino's Pepperoni Salami"],
+//   new: false,
+//   vegetarian: false,
+//   spicy: true,
+// };
 const Menu: NextPage = () => {
   return (
     <div className="container mx-auto ">
       <NavBar />
       <MenuList />
+      {/* <AboutPizza {...testProps} /> */}
     </div>
   );
 };
