1fb92a3392d607e56ed34e8257e5e257a4c65717
katisd <90249534+katisd@users.noreply.github.com>
2022-12-18 17:57:06 +0700

    🚚 move file ✏️ add headline in landing page

--- src/Components/Landing.tsx
+++ src/Components/Landing.tsx
@@ -18,6 +18,7 @@ const imgVariants = {
 const Landing = () => {
   return (
     <div className=" relative mx-auto flex h-screen justify-end overflow-hidden bg-base-100">
+      {/* picture */}
       <motion.img
         variants={imgVariants}
         initial="hidden"
@@ -31,6 +32,9 @@ const Landing = () => {
       {/* right pad */}
       <div className="z-10 flex min-h-screen w-1/2 flex-col items-center justify-center bg-base-100">
         <h1 className="text-7xl font-bold">Domino&apos;s Pizza</h1>
+        <p className="mt-5 text-2xl font-semibold">
+          Join us for a slice (or two) of heaven
+        </p>
         <Link
           href={"/menu"}
           className="btn-secondary btn-lg btn mt-10 rounded-full "
