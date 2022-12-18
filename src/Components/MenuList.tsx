e6096233d387cf55d983c81977e43fc4b46ccc03
katisd <90249534+katisd@users.noreply.github.com>
2022-12-18 08:29:54 +0700

    ✏️ change file name

--- src/Components/MenuList.tsx
+++ src/Components/MenuList.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "./Card";
 import data from "../../public/pizzaList.json";
-const Menu = () => {
+const MenuList = () => {
   return (
     <div className=" grid grid-cols-1 gap-10  px-5 pt-[80px]  md:grid-cols-2 lg:grid-cols-3">
       {data.map((x) => (
@@ -11,4 +11,4 @@ const Menu = () => {
   );
 };
 
-export default Menu;
+export default MenuList;
