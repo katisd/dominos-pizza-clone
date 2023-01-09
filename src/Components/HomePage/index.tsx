2ae1c1ca245ee21a84cab312da07084f6803762e
katisd <katidd17@gmail.com>
2023-01-09 21:05:09 +0700

    🩹 size of Image

--- src/Components/HomePage/index.tsx
+++ src/Components/HomePage/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import Image from "next/image";
 const imgVariants = {
   hidden: {
     opacity: 0,
@@ -21,16 +22,20 @@ const HomePage: React.FC = () => {
   return (
     <div className="relative flex h-screen flex-col overflow-hidden  bg-base-100 sm:items-center md:items-start ">
       {/* picture */}
-      <motion.img
+      <motion.div
         variants={imgVariants}
         initial="hidden"
         animate="visible"
         className=" mt-10 h-[50%] overflow-clip object-cover  md:absolute md:left-8 md:top-0 md:ml-[30vw] md:min-h-screen md:w-full"
-        src="/PizzaLanding.jpg"
-        alt="Pizza image"
-        width={200}
-        height={200}
-      />
+      >
+        <Image
+          className="h-full object-cover"
+          src="/PizzaLanding.jpg"
+          alt="Pizza image"
+          width={1000}
+          height={1000}
+        />
+      </motion.div>
       {/* right pad */}
       <div className="z-10 my-auto flex flex-col items-center justify-center bg-base-100 text-center md:min-h-screen md:w-1/2 ">
         <h1 className="text-5xl font-bold">Domino&apos;s Pizza</h1>
